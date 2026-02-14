2fc10301749a291e7e28bdeb4557f95a08ce9059
Aflah012 <167260074+Aflah012@users.noreply.github.com>
2026-02-14 08:39:17 +0700

    Update SettingsScreen.tsx
    Change Malaysia to Melayu

--- src/screens/SettingsScreen/SettingsScreen.tsx
+++ src/screens/SettingsScreen/SettingsScreen.tsx
@@ -71,7 +71,7 @@ const languageNames: Record<AvailableLanguage, string> = {
   //es: 'Español (ES)',
   //de: 'Deutsch (DE)',
   id: 'Indonesia (ID)',
-  ms: 'Malaysia (MS)',
+  ms: 'Melayu (MS)',
   ja: '日本語 (JA)',
   //ko: '한국어 (KO)',
   //pl: 'Polski (PL)',
